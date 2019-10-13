cce07eebb637e02ba69193a939f4ff0acfa620d3
JoeEverest <everestjoseph11@yahoo.com>
2019-10-13 14:56:37 +0300

    fixed session issue on branch

--- branch.php
+++ branch.php
@@ -1,5 +1,13 @@
 <?php
+session_start();
 include('config/config.php');
+
+if (isset($_SESSION['operator_name'])) {
+    $userLoggedIn = $_SESSION['operator_name'];   
+}
+else{
+	header("Location: login.php");
+}
 
 ?>
 <!DOCTYPE html>
