48c1afd3cb4825982eff5a1d65c342f11fce1045
JoeEverest <everestjoseph11@yahoo.com>
2019-10-15 12:29:02 +0300

    loans

---
+++ new_loan.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>New Loan</p>
+    <a href="loan_new_customer.php">New Customer</a>
+    <a href="loan_existing_customer.php">Existing Customer</a>
+</body>
+</html>
