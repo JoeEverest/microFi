f19f76f0e76c385d012a01d5779f90136a3a7dd1
JoeEverest <everestjoseph11@yahoo.com>
2019-10-15 13:06:39 +0300

    loans

--- loan_existing_customer.php
+++ loan_existing_customer.php
@@ -1,3 +1,39 @@
 <?php
+session_start();
+include('config/config.php');
+
+if (isset($_SESSION['operator_name'])) {
+    $userLoggedIn = $_SESSION['operator_name'];   
+}
+else{
+	header("Location: login.php");
+}
+
+$retrieve = 'SELECT * FROM customers ORDER BY id DESC';
+$retrieve = mysqli_query($connect, $retrieve);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+    Customer Name:
+        <select required name="customer_name">
+            <?php
+            while ($row = mysqli_fetch_array($retrieve)) {
+                $id = $row['id'];
+                $name = $row['customer_name'];
+            
+            ?>
+            <option value="<?php echo $name.' '.$id; ?>"><?php echo $name; ?></option>
+            <?php } ?>
+        </select><br>
+    </form>
+</body>
+</html>
