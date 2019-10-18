f8429afaa74c16659dbed7a7ac8b7c0d11a0d32d
JoeEverest <everestjoseph11@yahoo.com>
2019-10-18 17:17:31 +0300

    bootstrap add html

--- excel/excel_import.php
+++ excel/excel_import.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
     <title>Document</title>
 </head>
 <body>
